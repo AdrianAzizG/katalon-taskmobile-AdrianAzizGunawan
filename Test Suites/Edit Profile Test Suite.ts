0bfc6de20c68aabc52467f9e5d4c6ba817d8c20d
aziz_ <azizadrian9@gmail.com>
2024-02-13 11:24:28 +0700

    Create Test Suite for Edit Profile

---
+++ Test Suites/Edit Profile Test Suite.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Edit Profile Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>164d94b2-fc61-4e47-bdb0-d573a2890187</testSuiteGuid>
+   <testCaseLink>
+      <guid>f78a5b6c-5764-4adc-a23d-44ac13c43bd7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Edit Name</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>966f46d1-6d8d-4513-87e7-2af0f9f9f6fe</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Users</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>966f46d1-6d8d-4513-87e7-2af0f9f9f6fe</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>name</value>
+         <variableId>f668b484-e84f-4180-9854-4d183dd241f5</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>df47478f-b28e-4c6f-902e-989b70dc1ac6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Edit Email</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>612cf24f-e833-4de4-9821-67f1440208f9</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Users</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>612cf24f-e833-4de4-9821-67f1440208f9</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>email</value>
+         <variableId>917c8aa2-e7f8-475f-954d-5f524c312315</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
